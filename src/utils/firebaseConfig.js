ccf3bcf91c32b547d6ab481f722f382f99701e74
Ellie Lee <as152808@gmail.com>
2024-09-14 20:52:24 +0800

    chore: firebase config

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "becca-24.firebaseapp.com",
+  projectId: "becca-24",
+  storageBucket: "becca-24.appspot.com",
+  messagingSenderId: "61659824584",
+  appId: "1:61659824584:web:77831a4c5335d4ef25c0fd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
