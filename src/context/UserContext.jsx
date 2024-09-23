63d8a802de5d13d1a787b36c68cf9a96db989828
Ellie Lee <as152808@gmail.com>
2024-09-23 15:35:18 +0800

    fix: update user active days

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { getUserDocument } from "../utils/api";
-import { onAuthStateChanged, updateUserActiveDay } from "firebase/auth"; // 引入 Firebase 的監聽函數
+import { getUserDocument, updateActiveDays } from "../utils/api";
+import { onAuthStateChanged } from "firebase/auth"; // 引入 Firebase 的監聽函數
 import { auth } from "../utils/firebaseConfig";
 
 const UserContext = createContext();
@@ -16,7 +16,7 @@ export const UserProvider = ({ children }) => {
           const userData = await getUserDocument(currentUser.uid);
           console.log("獲取已登入用戶資料：", userData);
           setUser(userData);
-          await updateUserActiveDay(currentUser.uid);
+          await updateActiveDays(currentUser.uid);
         } catch (error) {
           console.error("獲取用戶資料失敗：", error);
         }
