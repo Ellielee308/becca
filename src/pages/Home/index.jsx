031f622a78afcbc6a8a02ea2e008ff7edbe7f940
Ellie Lee <as152808@gmail.com>
2024-10-05 17:50:17 +0800

    feat: add CardSetWrapper

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -45,7 +45,6 @@ function Home() {
               <FeatureTitle>自定義字卡樣式</FeatureTitle>
               <FeatureImage src={customizeSVG} />
             </ImageContainer>
-
             <FeatureDescription>
               為你的學習創造專屬的字卡樣式。BECCA
               讓你可以自由選擇字卡的顏色、字體及佈局，打造屬於自己的個性化學習工具，讓記憶過程更有趣、更有效。
@@ -56,7 +55,6 @@ function Home() {
               <FeatureTitle>智能建議內容</FeatureTitle>
               <FeatureImage src={searchSVG} />
             </ImageContainer>
-
             <FeatureDescription>
               新增字卡內容時，BECCA
               會根據你的輸入提供智能建議，幫助你快速找到最合適的翻譯詞彙，大幅節省字卡創建的時間。
@@ -67,7 +65,6 @@ function Home() {
               <FeatureTitle>多樣化測驗模式</FeatureTitle>
               <FeatureImage src={quizSVG} />
             </ImageContainer>
-
             <FeatureDescription>
               檢視學習成果的最佳方式就是測驗！BECCA
               提供配對題和選擇題兩種測驗模式，讓你在互動中掌握知識，檢驗自己的學習成效，同時增加學習的趣味性。
@@ -90,15 +87,12 @@ function Home() {
           探索熱門卡牌組
           <SectionEnglishSpan>Explore popular sets</SectionEnglishSpan>
         </SectionTitle>
-        <ExploreTitle>
-          英語<ExploreEnglishSpan>English</ExploreEnglishSpan>
-        </ExploreTitle>
-        <ExploreTitle>
-          日語<ExploreEnglishSpan>Japanese</ExploreEnglishSpan>
-        </ExploreTitle>
-        <ExploreTitle>
-          韓文<ExploreEnglishSpan>Korean</ExploreEnglishSpan>
-        </ExploreTitle>
+        <ExploreTitle>英語</ExploreTitle>
+        <CardSetWrapper></CardSetWrapper>
+        <ExploreTitle>日語</ExploreTitle>
+        <CardSetWrapper></CardSetWrapper>
+        <ExploreTitle>韓文</ExploreTitle>
+        <CardSetWrapper></CardSetWrapper>
       </ExploreSection>
       {showLoginModal && (
         <LoginModal onClose={() => setShowLoginModal(false)} />
@@ -264,9 +258,9 @@ const FeatureDescription = styled.p`
 `;
 
 const ExploreTitle = styled.div`
-  font-size: 20px;
+  font-size: 18px;
   font-family: "Noto Sans TC", sans-serif;
-  font-weight: 500;
+  font-weight: 400;
   color: #22254c;
 `;
 const ExploreEnglishSpan = styled.span`
@@ -279,8 +273,18 @@ const ExploreSection = styled.div`
   display: flex;
   flex-direction: column;
   padding: 30px 60px 30px 60px;
-  height: 600px;
   background-color: #e1e5f2;
+`;
+
+const CardSetWrapper = styled.div`
+  display: flex;
+  height: 280px;
+  width: 100%;
+  outline: 1px solid black;
+  margin: 20px 0;
+  &:last-of-type {
+    margin: 20px 0 0 0;
+  }
 `;
 
 const LoginModal = ({ onClose }) => {
@@ -456,302 +460,4 @@ const ErrorText = styled.p`
 
 LoginModal.propTypes = {
   onClose: PropTypes.func,
-};
-
-function CardContent({ currentStyle, currentTemplate, currentCard }) {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleFlip = () => {
-    setIsFlipped((prevState) => !prevState);
-  };
-  useEffect(() => {
-    setIsFlipped(false);
-  }, [currentCard]);
-
-  return (
-    <CardViewWrapper onClick={handleFlip}>
-      <FlipCard isFlipped={isFlipped} currentStyle={currentStyle}>
-        <FrontCard
-          isFlipped={isFlipped}
-          currentStyle={currentStyle}
-          currentCard={currentCard}
-        >
-          {currentCard &&
-            currentTemplate.frontFields.map((field, index) => {
-              const currentFrontField = currentCard.frontFields[index];
-              return (
-                <FieldContainer
-                  key={index}
-                  style={field.style}
-                  position={field.position}
-                >
-                  {renderFieldContent(
-                    field,
-                    currentFrontField ? currentFrontField.value : ""
-                  )}
-                </FieldContainer>
-              );
-            })}
-        </FrontCard>
-        <BackCard isFlipped={isFlipped} currentStyle={currentStyle}>
-          {currentCard &&
-            currentTemplate.backFields.map((field, index) => {
-              const currentBackField = currentCard.backFields[index];
-              return (
-                <FieldContainer
-                  key={index}
-                  style={field.style}
-                  position={field.position}
-                >
-                  {renderFieldContent(
-                    field,
-                    currentBackField ? currentBackField.value : ""
-                  )}
-                </FieldContainer>
-              );
-            })}
-        </BackCard>
-      </FlipCard>
-    </CardViewWrapper>
-  );
-}
-
-const renderFieldContent = (field, value) => {
-  switch (field.type) {
-    case "text":
-      return value;
-
-    case "image":
-      if (value && value.trim() !== "") {
-        return (
-          <ImageWrapper>
-            <Image src={value} alt={field.name} style={field.style} />
-          </ImageWrapper>
-        );
-      }
-      return null;
-
-    default:
-      return null;
-  }
-};
-
-const CardViewWrapper = styled.div`
-  align-self: center;
-  display: block;
-  margin: 20px 0px;
-  width: 600px;
-  height: 400px;
-  perspective: 1000px;
-  transform-style: preserve-3d;
-  cursor: pointer;
-`;
-
-const FlipCard = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  transform-style: preserve-3d;
-  transition: ${(props) => {
-    switch (props.currentStyle.animation) {
-      case "fade":
-        return "opacity 0.5s ease-in-out";
-      default:
-        return "all 0.5s ease-in-out";
-    }
-  }};
-  transform: ${(props) => {
-    switch (props.currentStyle.animation) {
-      case "horizontalFlip":
-        return props.isFlipped ? "rotateY(180deg)" : "rotateY(0)";
-      case "fade":
-        return "none";
-      default: // "vertical"
-        return props.isFlipped ? "rotateX(180deg)" : "rotateX(0)";
-    }
-  }};
-  border-radius: ${(props) => props.currentStyle.borderRadius};
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.15);
-  &:hover {
-    transform: ${(props) => {
-      switch (props.currentStyle.animation) {
-        case "horizontalFlip":
-          return props.isFlipped ? "rotateY(175deg)" : "rotateY(2deg)";
-        case "fade":
-          return "none";
-        default: // "vertical"
-          return props.isFlipped ? "rotateX(175deg)" : "rotateX(2deg)";
-      }
-    }};
-    box-shadow: 0 20px 20px rgba(50, 60, 60, 0.2);
-  }
-`;
-
-const FrontCard = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-  width: 100%;
-  height: 100%;
-  outline-style: ${(props) => props.currentStyle.borderStyle};
-  outline-color: ${(props) => props.currentStyle.borderColor};
-  outline-width: ${(props) => props.currentStyle.borderWidth};
-  background-color: ${(props) => props.currentStyle.backgroundColor};
-  border-radius: ${(props) => props.currentStyle.borderRadius};
-  backface-visibility: hidden;
-  font-family: ${(props) => props.currentStyle.fontFamily};
-  font-size: 32px;
-  opacity: ${(props) =>
-    props.currentStyle.animation === "fade" && props.isFlipped ? 0 : 1};
-  transition: ${(props) =>
-    props.currentStyle.animation === "fade"
-      ? "opacity 0.5s ease-in-out"
-      : "none"};
-`;
-
-const BackCard = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-  width: 100%;
-  height: 100%;
-  outline-style: ${(props) => props.currentStyle.borderStyle};
-  outline-color: ${(props) => props.currentStyle.borderColor};
-  outline-width: ${(props) => props.currentStyle.borderWidth};
-  background-color: ${(props) => props.currentStyle.backgroundColor};
-  border-radius: ${(props) => props.currentStyle.borderRadius};
-  backface-visibility: hidden;
-  font-family: ${(props) => props.currentStyle.fontFamily};
-  transform: ${(props) => {
-    switch (props.currentStyle.animation) {
-      case "horizontalFlip":
-        return "rotateY(180deg)";
-      case "fade":
-        return "none";
-      default: // "vertical"
-        return "rotateX(180deg)";
-    }
-  }};
-  font-size: 32px;
-  opacity: ${(props) =>
-    props.currentStyle.animation === "fade" ? (props.isFlipped ? 1 : 0) : 1};
-  transition: ${(props) =>
-    props.currentStyle.animation === "fade"
-      ? "opacity 0.5s ease-in-out"
-      : "none"};
-  z-index: ${(props) => (props.isFlipped ? 3000 : 0)};
-`;
-
-const FieldContainer = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: ${(props) => props.style.textAlign || "center"};
-  align-items: center;
-  ${(props) =>
-    props.style &&
-    `
-    width: ${props.style.width};
-    height: ${props.style.height};
-    font-size: ${props.style.fontSize};
-    font-weight: ${props.style.fontWeight};
-    color: ${props.style.color};
-    background-color: ${props.style.backgroundColor};
-  `};
-  left: ${(props) => props.position?.x || "0"}px;
-  top: ${(props) => props.position?.y || "0"}px;
-  user-select: none;
-`;
-
-// 用於顯示圖片的樣式
-const ImageWrapper = styled.div`
-  position: relative;
-  display: inline-block; // 讓 ImageWrapper 的大小與圖片保持一致
-`;
-
-const Image = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: ${(props) => props.style?.objectFit || "cover"};
-  display: block;
-`;
-
-CardContent.propTypes = {
-  currentTemplate: PropTypes.shape({
-    templateName: PropTypes.string.isRequired,
-    frontFields: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        type: PropTypes.oneOf(["text", "image"]).isRequired,
-        required: PropTypes.bool.isRequired,
-        position: PropTypes.shape({
-          x: PropTypes.number.isRequired,
-          y: PropTypes.number.isRequired,
-        }).isRequired,
-        style: PropTypes.shape({
-          width: PropTypes.string.isRequired,
-          height: PropTypes.string.isRequired,
-          fontSize: PropTypes.string,
-          fontWeight: PropTypes.string,
-          color: PropTypes.string,
-          textAlign: PropTypes.string,
-        }).isRequired,
-      })
-    ).isRequired,
-    backFields: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        type: PropTypes.oneOf(["text", "image"]).isRequired,
-        required: PropTypes.bool.isRequired,
-        position: PropTypes.shape({
-          x: PropTypes.number.isRequired,
-          y: PropTypes.number.isRequired,
-        }).isRequired,
-        style: PropTypes.shape({
-          width: PropTypes.string.isRequired,
-          height: PropTypes.string.isRequired,
-          fontSize: PropTypes.string,
-          fontWeight: PropTypes.string,
-          color: PropTypes.string,
-          textAlign: PropTypes.string,
-        }).isRequired,
-      })
-    ).isRequired,
-  }).isRequired,
-  currentStyle: PropTypes.shape({
-    styleId: PropTypes.string,
-    userId: PropTypes.string.isRequired,
-    styleName: PropTypes.string.isRequired,
-    borderStyle: PropTypes.oneOf(["none", "solid", "dashed", "dotted"]),
-    borderColor: PropTypes.string,
-    borderWidth: PropTypes.string,
-    borderRadius: PropTypes.string,
-    backgroundColor: PropTypes.string.isRequired,
-    fontFamily: PropTypes.string.isRequired,
-    animation: PropTypes.oneOf(["verticalFlip", "horizontalFlip", "fade"])
-      .isRequired,
-  }).isRequired,
-  currentCard: PropTypes.shape({
-    cardId: PropTypes.string.isRequired,
-    cardSetId: PropTypes.string.isRequired,
-    userId: PropTypes.string.isRequired,
-    frontFields: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    backFields: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    createdAt: PropTypes.string.isRequired,
-  }).isRequired,
 };
