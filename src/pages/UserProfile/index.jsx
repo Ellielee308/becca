a344295fb27a751ff612bf5e5324e345d48b9007
Ellie Lee <as152808@gmail.com>
2024-10-23 11:03:07 +0800

    refactor: move SVG components

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -5,6 +5,7 @@ import "react-calendar/dist/Calendar.css";
 import styled, { keyframes } from "styled-components";
 import {
   CalendarIcon,
+  ProfileEditIcon as EditIcon,
   EditNameIcon,
   ProfileIcon,
   ProfileStarIcon as StarIcon,
@@ -460,17 +461,6 @@ const EditProfilePictureIcon = styled.div`
   box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
   cursor: pointer;
 `;
-const EditIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    width="20"
-    height="20"
-  >
-    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-  </svg>
-);
 
 const ModalOverlay = styled.div`
   position: fixed;
